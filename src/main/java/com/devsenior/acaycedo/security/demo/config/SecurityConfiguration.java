d3b4d9f71064649f39793133b9a49754b969d995
acaycedo <afcaycedo14@gmail.com>
2025-11-14 08:19:07 -0500

    EndPoints Con roles

--- src/main/java/com/devsenior/acaycedo/security/demo/config/SecurityConfiguration.java
+++ src/main/java/com/devsenior/acaycedo/security/demo/config/SecurityConfiguration.java
@@ -2,8 +2,14 @@ package com.devsenior.acaycedo.security.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 //@Configuration Etiqueta para indicar que esta clase se inicia antes de cualquier cosa
@@ -27,11 +33,37 @@ public class SecurityConfiguration {
         http.csrf(crsf -> crsf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/","index.html").permitAll()
-                        .requestMatchers("/api/hello").permitAll()
-                        .anyRequest().authenticated())
+                        //.requestMatchers("/api/hello").permitAll()
+                        //Implementacion de HTTP metodos para permitir acciones
+                        // para roles dependiendo la peticion HTTP.
+                        .requestMatchers(HttpMethod.GET).hasAnyRole("ADMIN","USER")
+                        .requestMatchers(HttpMethod.POST).hasRole("ADMIN")
+                        .anyRequest().denyAll())
                 //.formLogin(log -> {});
                 .httpBasic(basic -> {});
                 
         return http.build();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder){
+
+        var user1 = User.builder()
+        .username("acaycedo")
+        .password(passwordEncoder.encode("admin123"))
+        .authorities("ROLE_ADMIN")// Esto viene ya preestablecido con un ENUM con roles básicos
+        .build();
+
+        var user2 = User.builder()
+        .username("ajimenez")
+        .password(passwordEncoder.encode("admin12"))
+        .authorities("ROLE_USER")// Esto viene ya preestablecido con un ENUM con roles básicos
+        .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
--- src/main/java/com/devsenior/acaycedo/security/demo/controller/HelloWorldController.java
+++ src/main/java/com/devsenior/acaycedo/security/demo/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.devsenior.acaycedo.security.demo.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,10 @@ public class HelloWorldController {
     @GetMapping
     public Map<String, Object> getHello(){
         return Map.of("message:","HelloWorld");
+    }
+
+    @PostMapping
+    public Map<String, Object> postHello(@RequestBody Map<String, Object> body){
+        return Map.of("message:","Hello "+body.get("name"));
     }
 }
